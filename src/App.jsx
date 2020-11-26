6a827207acd980de002a86963ab3616c788b1662
Grazia Palombella <grazia.palombella@gmail.com>
2020-11-26 15:08:04 +0100

    App reloaded

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./style/App.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { ContextAPI } from "./context/ContextAPI";
+import { useEffect, useState } from "react";
+
+function App() {
+    const [results, setResults] = useState({});
+
+    useEffect(() => {
+        void (async () => {
+            const res = await (
+                await fetch(
+                    "http://51.77.82.133:86/api/quotations/QUO_5fb3acb3a0f18"
+                )
+            ).json();
+            setResults(res.results.data);
+        })();
+    }, []);
+
+    return (
+        <ContextAPI.Provider value={results}>
+            <div className="App">
+                <Header />
+                <Footer />
+            </div>
+        </ContextAPI.Provider>
+    );
+}
+
+export default App;
