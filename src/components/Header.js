0f4744ec9c9ff51beaeca656c7fdd6786427e0d8
GioBerl <giovanni.berl1@gmail.com>
2020-11-26 12:19:18 +0100

    Giovanni prova Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
-import React, {useContext} from 'react'
-import { ContextAPI } from '../context/ContextAPI'
+import React, { useContext } from "react";
+import { ContextAPI } from "../context/ContextAPI";
 
 function Header() {
-
-const resultsAPI = useContext(ContextAPI)
+    const resultsAPI = useContext(ContextAPI);
 
     return (
         <div>
             <h1>{resultsAPI.customerName}</h1>
             <h2>{resultsAPI.title}</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+                alias reprehenderit dolorum quod explicabo, unde animi id est
+                suscipit in qui, veniam impedit veritatis iusto aspernatur fugit
+                libero doloremque consequatur?
+            </p>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
