3b1a62ffcd1daf3b93041092bf18fcc8b68a731f
Nicolò <nicolomuscas@gmail.com>
2020-11-26 12:13:23 +0100

    add title to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ const resultsAPI = useContext(ContextAPI)
     return (
         <div>
             <h1>{resultsAPI.customerName}</h1>
+            <h2>{resultsAPI.title}</h2>
         </div>
     )
 }
